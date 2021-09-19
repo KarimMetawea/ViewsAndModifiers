c232b9c386ca8f8ed56f4137d133d12ecbb9b613
Karim Metawea <ks.metawea@mohre.gov.ae>
2021-09-19 12:21:01 +0400

    Initial Commit

---
+++ ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Karem on 9/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
